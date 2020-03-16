ac072e0c9d2ff53a2a516f68108f0e32b9677263
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-03-16 16:57:02 -0300

    teste pro ronald

--- ObjectiveC_challenge/ViewController.h
+++ ObjectiveC_challenge/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
-
+@property (nonatomic) NSString *API_KEY;
+@property (nonatomic) NSString *popularURL;
 @end
 
--- ObjectiveC_challenge/ViewController.m
+++ ObjectiveC_challenge/ViewController.m
@@ -10,13 +10,34 @@
 
 @interface ViewController ()
 
+
+
 @end
 
 @implementation ViewController
 
+// url example: https://api.themoviedb.org/3/movie/550?api_key=6af6fb6deb5f2e4c6d36e514240eeebb
+
+
+NSString *base_url ;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.API_KEY =  @"6af6fb6deb5f2e4c6d36e514240eeebb";
+    self.popularURL = @"https://api.themoviedb.org/3/movie/popular?";
+    base_url = [NSString stringWithFormat: @"%@%@", _popularURL, _API_KEY];
+    
+    
+    self.view.backgroundColor = UIColor.blackColor;
+    [self fetchMovies];
+}
+
+- (void) fetchMovies {
+    NSLog(@"Fetching movies");
+    NSLog(base_url);
+    NSString *url = @"";
 }
 
 
