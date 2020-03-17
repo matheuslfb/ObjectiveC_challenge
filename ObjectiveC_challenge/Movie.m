3619cc5113aeea5f0857d165c785b0cae7dd483a
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-03-17 17:20:45 -0300

    fix movie model

---
+++ ObjectiveC_challenge/Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  ObjectiveC_challenge
+//
+//  Created by Matheus Lima Ferreira on 3/17/20.
+//  Copyright © 2020 Matheus Lima Ferreira. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+    
+@end
