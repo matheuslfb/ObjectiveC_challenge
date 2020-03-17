e7fd67b86b491c4d708abb27b266154a553644e7
Ronald Maciel <ronaldsmaciel2@outlook.com>
2020-03-17 04:28:55 -0300

    Start the backbone layout of the main screen

---
+++ ObjectiveC_challenge/Controller/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  ObjectiveC_challenge
+//
+//  Created by Ronald Maciel on 17/03/20.
+//  Copyright © 2020 Matheus Lima Ferreira. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjectiveC_challenge/Controller/MainViewController.m
@@ -0,0 +1,30 @@
+//
+//  MainViewController.m
+//  ObjectiveC_challenge
+//
+//  Created by Ronald Maciel on 17/03/20.
+//  Copyright © 2020 Matheus Lima Ferreira. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+
+
+
+
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+
+
+@end
